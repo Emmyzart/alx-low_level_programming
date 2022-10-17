6006e849982d051af31a855a388fe598e4b570c0
Emmyzart <emekawilliams4christ@gmail.com>
2022-10-17 14:03:12 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	c--;
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}

7694a5f0d3a12543d4eef3771b340cd8446d69f9
Emmyzart <emekawilliams4christ@gmail.com>
2022-10-18 09:47:53 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z')
+	{
+		_putchar(c);
+	}
+	c++;
+
+	_putchar('\n');
+	return (0);
+}

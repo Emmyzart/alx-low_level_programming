2b85e4cadcce2d4aac31ff426f904df4c1dfd19b
Emmyzart <emekawilliams4christ@gmail.com>
2022-10-20 10:14:32 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/** main - entry of the program
+ *
+ * Return: end
+ */
+
+int main(void)
+{
+	char c;
+
+	if (c == 'A')
+	{
+	return (1);
+		}
+		if (c == 'a')
+		{
+		return (0);
+		}
+}

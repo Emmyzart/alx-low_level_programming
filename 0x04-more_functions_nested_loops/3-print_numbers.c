fbfbad11c25b74fff2a6b453e2c7dd3fc380a751
Emmyzart <emekawilliams4christ@gmail.com>
2022-10-20 16:48:58 -0700

    initial commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,15 @@
 /**
  * print_numbers - print numbers form 0 to 9
  * _putchar - reference
- * Return: 0
+ * Return: none
  */
 void print_numbers(void)
 {
 	int c;
 
 	for (c = 0; c < 10; c++)
-	_putchar(c);
+	{
+		_putchar(c);
+	}
 	_putchar('\n');
 }

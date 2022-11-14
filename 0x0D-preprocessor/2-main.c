408341bc5191fa57060fdb0c9371aeedf84c903d
Emmyzart <emekawilliams4christ@gmail.com>
2022-11-14 15:20:48 -0800

    initial commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-/*
-* main - entry of the program
-*
-* Return: 0
-*/
-
+/**
+ * main - prints the name of the file.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	printf("%s\n", __FILE__);
+	printf("%s\n", FILE);
 	return (0);
 }
